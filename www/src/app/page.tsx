e5218b3f648ebf314d19ce3ae0791bd2b87d4602
Alex Werner <obusco@gmail.com>
2024-02-01 20:13:08 +0100

    feat(www): provide create-next-app base

---
+++ www/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="flex flex-col items-center justify-center">
+            <h1 className="text-6xl font-bold">SIWE Auth</h1>
+        </div>
+    </main>
+  );
+}
